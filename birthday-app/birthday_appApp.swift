c4cbc5b9258f43656592cec50b9cd98264dc1c76
Meredith Heller <meredith.heller.vabzby@statefarm.com>
2022-07-16 15:34:56 -0400

    Initial Commit

---
+++ birthday-app/birthday_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  birthday_appApp.swift
+//  birthday-app
+//
+//  Created by Meredith Heller on 7/16/22.
+//
+
+import SwiftUI
+
+@main
+struct birthday_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
